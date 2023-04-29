b737d6c5d9bf04a0442340fd6a5a74289993280c
BenKoenig <bkoenig.as@gmail.com>
2023-04-29 15:48:13 +0200

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,8 @@
 import CanvasWrapper from "../components/CanvasWrapper.jsx";
-import StatsGUI from "../components/StatsGUI.jsx";
 
 const Home = () => {
     return(
-        <>
-            <StatsGUI/>
-            <CanvasWrapper/>
-        </>
+        <CanvasWrapper/>
     )
 }
 
--- src/router.jsx
+++ src/router.jsx
@@ -1,14 +1,15 @@
+import './App.css'
 import { Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
+import Home from "./pages/Home.jsx";
 
-const App = () => {
+function router() {
     return (
         <>
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
         </>
-    );
-};
+    )
+}
 
-export default App;
+export default router
