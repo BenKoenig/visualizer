6818c20bf64f66669ed17786d93caf1469851700
BenKoenig <bkoenig.as@gmail.com>
2023-06-13 16:04:10 +0200

    refactor

--- src/components/canvas/CanvasWrapper.tsx
+++ src/components/canvas/CanvasWrapper.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable */
 // @ts-nocheck
 import React from 'react'
-import { Canvas,extend } from '@react-three/fiber'
-import { OrbitControls, PerspectiveCamera, Plane, Cloud, MeshReflectorMaterial } from '@react-three/drei'
+import { Canvas,extend  } from '@react-three/fiber'
+import { OrbitControls, Sphere, Plane, SpotLight, MeshReflectorMaterial, Sky } from '@react-three/drei'
 import GrassGroundPlane from './GrassGroundPlane'
 import * as THREE from 'three'
 import ModelFbx from './ModelFbx'
@@ -12,6 +12,7 @@ extend({ Plane, MeshReflectorMaterial });
 const CanvasWrapper: React.FC = () => {
   const controlsRef = React.useRef<any>(null);
 
+  const lightPosition = [0, 50, 20];
   const handleControlsChange = () => {
     const controls = controlsRef.current
     if (controls) {
@@ -26,41 +27,38 @@ const CanvasWrapper: React.FC = () => {
 
   return (
     <Canvas
-      gl={{
-        antialias: true,
-        toneMapping: THREE.ACESFilmicToneMapping,
-        outputEncoding: THREE.sRGBEncoding
-      }}
+      shadowMap
+
       style={{ width: '100%', height: '100%' }}
       camera={{ position: [0, 6, 15] }} // Adjust the camera position here
     >
-      <scene>
-        <mesh>
-          <boxGeometry args={[100, 100, 100]} />
-          <meshBasicMaterial color="skyblue" side={THREE.BackSide} />
-        </mesh>
-      </scene>
+
 
       {/* <ambientLight intensity={0.5} /> */}
+      <Sky sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} />
       <pointLight position={[0, 20, 10]} intensity={1.5} />
+      <ambientLight intensity={0.8} />
 
-      <Cloud
+   {/*    <Cloud
         opacity={0.15}
         speed={0.4} // Rotation speed
         width={10} // Width of the full cloud
         depth={1.5} // Z-dir depth
         segments={22} // Number of particles
         position={[0, 10, 0]}
-      />
+      /> */}
 
       <GrassGroundPlane />
-      <group>
-        <ModelFbx url="/palm.fbx" pos={[-4,-1,-1]} scale={0.018} />
-        <ModelFbx url="/palm.fbx" pos={[2,-1,0]} scale={0.018} />
-      </group>
+
+      <ModelFbx url="/palm.fbx" pos={[2,-1,0]} scale={0.018} />
 
       <OrbitControls ref={controlsRef} onChange={handleControlsChange} />
-      <fog attach="fog" args={['#000', 10, 50]} />
+      <SpotLight position={lightPosition} angle={0.3} castShadow />
+
+      {/* Add a small sphere to represent the position of the light */}
+      <Sphere args={[1, 16, 16]} position={lightPosition}>
+        <meshBasicMaterial color="yellow" />
+      </Sphere>
 
     </Canvas>
   )
