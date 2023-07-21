f83609cc1c5e3679e873c46805e0b9398472737b
BenKoenig <bkoenig.as@gmail.com>
2023-07-21 15:41:36 +0200

    rename file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import CanvasWrapper from '../components/canvas/CanvasWrapper'
 import Gui from '../components/gui/Gui'
-import Hud from '../components/hud/Hud'
+import LoadingBar from '../components/loadingBar/LoadingBar'
 /* import Loading from '../components/loading/Loading' */
 
 const Home: React.FC = () => {
   return (
         <>
      {/*        <Loading/> */}
-            <Hud/>
+            <LoadingBar/>
             <Gui/>
             <CanvasWrapper/>
         </>
