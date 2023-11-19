7782a10a8b1e82ad705effb6782b00ea413c7cff
BenKoenig <bkoenig.as@gmail.com>
2023-11-19 12:58:06 +0100

    Update loadign screen

--- src/components/loadingScreen/LoadingScreen.tsx
+++ src/components/loadingScreen/LoadingScreen.tsx
@@ -13,7 +13,7 @@ const LoadingScreen: React.FC = () => {
           </div>
         </div>
         <div className={classes.container__infoWrapper}>
-          <p>If Vienna were a rainforest, it would only take 30 days for it to be completely deforested.</p>
+          <p>If Vienna, known for its urban and cultural landscape, were a rainforest, it would be completely deforested in just 30 days, demonstrating the alarming rate at which deforestation occurs in actual rainforests.</p>
           <p><i>Loading content...</i></p>
         </div>
       </div>
